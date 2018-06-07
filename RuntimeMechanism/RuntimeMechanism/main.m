378a224a79b457dd5040b132a1c5c773ced22cb6
Flyfishering <2426715336@qq.com>
2018-06-07 15:54:48 +0800

    项目初始化

---
+++ RuntimeMechanism/RuntimeMechanism/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RuntimeMechanism
+//
+//  Created by seasaw on 2018/6/7.
+//  Copyright © 2018年 wangbinbin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
